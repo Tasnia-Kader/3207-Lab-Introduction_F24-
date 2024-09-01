492343494d4609813770d9249025ee11fb742a53
Tasnia-Kader <tuo56603@temple.edu>
2024-08-31 21:57:18 -0500

    Created the randchar() function

---
+++ random.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char randchar()
+{
+    // Generate rand num from A (65) to Z (90):
+    // 0 <= rand() % 26 <= 25; 65 <= rand() % 26 + 65 <= 90
+
+    int integer = rand() % 26 + 65;
+
+    return (char)integer;
+}
